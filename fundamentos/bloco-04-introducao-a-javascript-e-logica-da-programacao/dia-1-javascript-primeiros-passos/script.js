39ccd6abe9165f458488e4d05959371a0f25ca8e
Renan Storrer <storrerrenan@gmail.com>
2022-06-21 18:58:04 -0300

    fazendo os 3 primeiros exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,35 @@
+// exercicio 1
+
+const a = 2;
+const b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// exercicio 2
+
+const c = 5;
+const d = 10;
+
+if (c > d) {
+    console.log(c)
+} else {
+    console.log(d)
+}
+
+// exercicio 3
+
+const e = 25;
+const f = 25.1;
+const g = 24.9;
+
+if (e > f && e > g) {
+    console.log(e)
+} else if (f > g && f > e) {
+    console.log(f)
+} else if (g > e && g > f){
+    console.log(g)
+}
