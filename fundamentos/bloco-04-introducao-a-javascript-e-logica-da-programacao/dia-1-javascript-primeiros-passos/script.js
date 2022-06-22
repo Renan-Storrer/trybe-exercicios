f5025971b95231a6cc2a217200c9dcf47be51167
Renan Storrer <storrerrenan@gmail.com>
2022-06-22 01:00:08 -0300

    exercicio 11

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -161,3 +161,36 @@ if (custo >= 0 && valor >= 0) {
 } else {
     console.log("ERRO: valores negativos!");
 }
+
+// exercicio 11 
+
+let inss;
+let ir;
+
+let salarioBruto = 3500.00;
+
+if (salarioBruto <= 1556.94) {
+    inss = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {     
+    inss = salarioBruto * 0.09
+} else if (salarioBruto <= 5189.82) {     
+    inss = salarioBruto * 0.11;
+} else {
+    inss = 570.88;
+}
+
+let salarioBase = salarioBruto - inss;
+
+if (salarioBase <= 1903.98) {
+    ir = 0;
+} else if (salarioBase <= 2826.65) {
+    ir = (salarioBase * 0.075) - 142.8;
+} else if (salarioBase <= 3751.05) {
+    ir = (salarioBase * 0.15) - 354.8;
+} else if (salarioBase <= 4664.68) {
+    ir = (salarioBase * 0.225) - 636.13;
+} else {
+    ir = (salarioBase * 0.275) - 869.36;
+}
+
+console.log("Salário Líquido: " + (salarioBase - ir));
