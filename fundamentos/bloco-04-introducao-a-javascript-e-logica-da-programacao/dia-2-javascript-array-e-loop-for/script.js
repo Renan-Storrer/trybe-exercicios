26bd33c7f547d898db077468548b9c632dca6936
Renan Storrer <storrerrenan@gmail.com>
2022-06-22 18:44:09 -0300

    exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -5,3 +5,14 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 }
+
+// exercicio 2 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    resultado += numbers[index];
+}
+
+console.log(resultado);
