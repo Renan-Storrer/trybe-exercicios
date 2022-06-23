a3f739a4a901500899e6adad46e2a2e5fed8d1b4
Renan Storrer <storrerrenan@gmail.com>
2022-06-23 15:25:36 -0300

    exercicio 9

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -93,10 +93,22 @@ console.log(menorValor);
 
 // exercicio 8
 
-let array = [];
+let numbers = [];
 
 for (let index = 1; index <= 25; index +=1) {
-    array.push(index)
+    numbers.push(index)
 }
 
-console.log(array)
+console.log(numbers)
+
+// exercicio 9
+
+let numbers = [];
+
+for (let index = 1; index <= 25; index +=1) {
+    numbers.push(index)
+}
+
+for (let index2 = 0; index2 < numbers.length; index2 +=1) {
+    console.log(numbers[index2] / 2);
+}
