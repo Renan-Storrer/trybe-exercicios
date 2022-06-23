7de196cb3e2b2700930a4315b3367fb8c0fc6d79
Renan Storrer <storrerrenan@gmail.com>
2022-06-23 19:30:01 -0300

    exercicio 1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -0,0 +1,13 @@
+// exercicio 1
+
+let n = 3;
+let simbolo = "*"
+let linha = "";
+
+for(index = 0; index < n; index += 1) {
+    linha = linha + simbolo
+}
+
+for (let index2 = 0; index2 < n; index2 += 1) {
+    console.log(linha)
+}
