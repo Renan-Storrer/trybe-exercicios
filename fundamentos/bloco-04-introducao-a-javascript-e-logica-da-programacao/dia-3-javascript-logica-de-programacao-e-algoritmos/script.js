791bfa14f78e6a1cb0e8425f621d0cd57d864f3f
Renan Storrer <storrerrenan@gmail.com>
2022-06-23 19:04:32 -0300

    exercicio 3, primeira parte

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -18,3 +18,16 @@ for (let index = 0; index < palavra.length; index += 1) {
 }
 
 console.log(palavraContrario)
+
+// exercicio 3 
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length > maiorPalavra.length) {
+        maiorPalavra = array[index]
+    }
+}
+
+console.log(maiorPalavra);
