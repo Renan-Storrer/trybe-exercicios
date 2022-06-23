d11da0789a5492b5bea7acc049c0b4dfac71c12a
Renan Storrer <storrerrenan@gmail.com>
2022-06-23 18:38:08 -0300

    exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -7,3 +7,14 @@ for (let index = 10; index > 0; index -= 1) {
 }
 
 console.log(fatorial);
+
+// exercicio 2
+
+let palavra = "Abacaxi"
+let palavraContrario = "";
+
+for (let index = 0; index < palavra.length; index += 1) {
+    palavraContrario = palavraContrario +  palavra[palavra.length - 1 - index]
+}
+
+console.log(palavraContrario)
