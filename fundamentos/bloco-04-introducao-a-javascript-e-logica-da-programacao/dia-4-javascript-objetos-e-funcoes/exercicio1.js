5b1c0b6597c2e1393435bcfe02dc3b72c89e080d
Renan Storrer <storrerrenan@gmail.com>
2022-06-24 18:39:52 -0300

    exercicio 7

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-4-javascript-objetos-e-funcoes/exercicio1.js
@@ -43,12 +43,22 @@ let leitor = {
     sobrenome: 'Pessoa',
     idade: 21,
     livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
     ],
-  };
+};
 
-  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + '"' + leitor.livrosFavoritos[0].titulo + '"') ; 
+//   console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + '"' + leitor.livrosFavoritos[0].titulo + '"') ; 
+
+leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+    },
+);
+
+console.log(leitor.livrosFavoritos)
