767e63f8b07629c695d323517c9d9d9e200bc7ef
Renan Storrer <storrerrenan@gmail.com>
2022-06-25 02:41:55 -0300

    exercicio 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-4-javascript-objetos-e-funcoes/exercicio2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-da-programacao/dia-4-javascript-objetos-e-funcoes/exercicio2.js
@@ -1,3 +1,5 @@
+// exercicio 1
+
 function verificaPalindrome(word) {
     for (index in word) {
         if (word[index] !== word[(word.length - 1) - index]) {
@@ -15,5 +17,19 @@ function verificaPalindrome(string) {
     }
 }
 
-console.log(verificaPalindrome('arara'));
-console.log(verificaPalindrome('desenvolvimento'));
+// console.log(verificaPalindrome('arara'));
+// console.log(verificaPalindrome('desenvolvimento'));
+
+// exercicio 2
+
+function indiceDoMaior(numeros) {
+    let indiceDoMaiorNumero = 0;
+    for (let index in numeros) {
+      if (numeros[indiceDoMaiorNumero] < numeros[index]) {
+        indiceDoMaiorNumero = index;
+      }
+    }
+    return indiceDoMaiorNumero;
+  }
+  
+  console.log(indiceDoMaior([2, 3, 6, 7, 10, 1]));
