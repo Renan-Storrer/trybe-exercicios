1bb5acc50edd9287e6558392308f25256fca820e
Renan Storrer <storrerrenan@gmail.com>
2022-06-28 18:26:34 -0300

    exercicio 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,5 +1,23 @@
+// exercicio 1
+
 function mudarTexto () {
     let paragrafo = document.getElementsByTagName('p')[1];
     paragrafo.innerText = 'Estar trabalhando em uma empresa que eu goste'
 }
 mudarTexto();
+
+// exercicio 2
+
+function mudarBackGround () {
+    let quadradoAmarelo = document.getElementsByClassName('main-content')[0];
+    quadradoAmarelo.style.background = 'rgb(76,164,109)';
+}
+
+mudarBackGround();
+
+function mudarBackGround2 () {
+    let quadradoVermelho = document.getElementsByClassName('center-content');
+    quadradoVermelho.style.background = 'white'
+}
+
+mudarBackGround2
