c9e56f8b888fa5549a136ad289502177462bc718
Renan Storrer <storrerrenan@gmail.com>
2022-06-29 19:00:56 -0300

    exercicio 2

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -3,3 +3,11 @@
 let h1 = document.createElement('h1')
 h1.innerText = "Exercício 5.2 - JavaScript DOM"
 document.body.appendChild(h1)
+
+// exercicio 2
+
+let main = document.createElement('main')
+main.className = 'main-content'
+document.body.appendChild(main)
+
+// exercicio 3
