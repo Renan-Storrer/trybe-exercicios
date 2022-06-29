f45a27c5e9e81fb98df69e274cd75d0de75125dc
Renan Storrer <storrerrenan@gmail.com>
2022-06-29 19:19:36 -0300

    exercicio 6 e 7

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -27,3 +27,14 @@ p.innerText = 'TEXTO TEXTO TEXTO TEXTO TEXTO'
 let sectionLeft = document.createElement('sextion')
 sectionLeft.className = 'left-contente';
 main.appendChild(sectionLeft)
+
+// exercicio 6
+
+let sectionRigth = document.createElement('sextion')
+sectionRigth.className = 'rigth-contente';
+main.appendChild(sectionRigth)
+
+let imagem = document.createElement('img')
+imagem.className = 'small-image'
+imagem.src = 'https://picsum.photos/200'
+section.appendChild(imagem)
