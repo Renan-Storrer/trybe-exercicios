1525ab8bfa523334ed4ba4f497da0280d991d463
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 03:25:36 -0300

    requisito 5

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -2,6 +2,7 @@ let body = document.querySelector('body');
 let h1Body = document.createElement('h1');
 let mainBody = document.createElement('main');
 let sectionMain = document.createElement('section')
+let sectionClass = document.createElement('section')
 let p = document.createElement('p')
 
 h1Body.innerText = 'Exercício 5.2 - JavaScript DOM'
@@ -16,8 +17,11 @@ mainBodyElement.appendChild(sectionMain)
 sectionMain.className = 'center-content'
 sectionMain.id = '3'
 // console.log(sectionMain);
+mainBodyElement.appendChild(sectionClass)
+sectionClass.className = 'left-content'
 
 let sectionMainElement = document.getElementById('3')
 console.log(sectionMainElement);
 sectionMainElement.appendChild(p)
 p.innerText = 'Olá, requisito 4'
+
