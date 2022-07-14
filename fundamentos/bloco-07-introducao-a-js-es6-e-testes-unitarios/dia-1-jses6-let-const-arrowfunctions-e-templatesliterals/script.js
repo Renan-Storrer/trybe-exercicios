f95f454ffb81ea4bad71f25925330b1244bbedf9
Renan Storrer <storrerrenan@gmail.com>
2022-07-14 18:50:27 -0300

    ex 2

--- fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/script.js
+++ fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/script.js
@@ -1,3 +1,5 @@
+// exercicio 1
+
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,3 +12,17 @@ const testingScope = (escopo) => {
 }
 
 testingScope(true);
+
+// exercicio 2
+
+function sortFunction(a, b) {
+  return (a - b)
+}
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const numberOrder = oddsAndEvens.sort(sortFunction);
+
+const mensagemFinal = `Os números ${numberOrder} se encontram ordenados de forma crescente!`
+
+console.log(mensagemFinal);
