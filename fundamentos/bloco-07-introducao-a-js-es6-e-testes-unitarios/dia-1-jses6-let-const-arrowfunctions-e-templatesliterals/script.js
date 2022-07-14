947b7ff2d863eb79137f3f2fafbe3a6aec8f1aeb
Renan Storrer <storrerrenan@gmail.com>
2022-07-14 18:42:25 -0300

    ex 1.3

--- fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/script.js
+++ fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/script.js
@@ -1,10 +1,10 @@
 const testingScope = (escopo) => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
     console.log(ifScope);
   } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
 }
