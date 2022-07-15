8d02a4858900456ce2b3099656d16d8db549bbe7
Renan Storrer <storrerrenan@gmail.com>
2022-07-15 01:14:55 -0300

    faz função 2 do ex04

--- fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/ex04/script.js
+++ fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/ex04/script.js
@@ -1,18 +1,39 @@
-function ola (nome) {
-  let nomeDesejado = 'Tryber x aqui!';
-  let nomeSeparado = nomeDesejado.split(' ');
+// função 1
+
+function ola(nome) {
+  let frase = 'Tryber x aqui!';
+  let fraseSeparada = frase.split(' ');
   let nomeFor = []
   // console.log(nomeSeparado);
-  for (let i = 0; i < nomeSeparado.length; i += 1) {
-    if (nomeSeparado[i] !== 'x') {
-      nomeFor.push(nomeSeparado[i])
+  for (let i = 0; i < fraseSeparada.length; i += 1) {
+    if (fraseSeparada[i] !== 'x') {
+      nomeFor.push(fraseSeparada[i])
     } else {
       nomeFor.push(nome)
     }
   }
   // console.log(nomeFor);
-  nomeFinal = nomeFor.join(' ')
-  console.log(nomeFinal);
+  let nomeFinal = nomeFor.join(' ')
+  // console.log(nomeFinal);
+  return nomeFinal
 }
 
-ola('Renan');
+// ola('Renan');
+
+// função 2
+
+const minhasSkills = (func) => {
+  const skills = ['HTML', 'CSS', 'JavaScript'];
+  let resultado = `
+  ${func}
+  
+  Minhas três principais habilidades são:`;
+
+  skills.forEach((skills) => {
+    resultado = `${resultado}
+    - ${skills}`;
+  });
+  return resultado
+}
+
+console.log(minhasSkills(ola('renan')));
