89fd6a088fec539f8cab8f7d07f66c8117dc8959
Renan Storrer <storrerrenan@gmail.com>
2022-07-15 00:55:13 -0300

    faz função 1 do ex04

---
+++ fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/ex04/script.js
@@ -0,0 +1,18 @@
+function ola (nome) {
+  let nomeDesejado = 'Tryber x aqui!';
+  let nomeSeparado = nomeDesejado.split(' ');
+  let nomeFor = []
+  // console.log(nomeSeparado);
+  for (let i = 0; i < nomeSeparado.length; i += 1) {
+    if (nomeSeparado[i] !== 'x') {
+      nomeFor.push(nomeSeparado[i])
+    } else {
+      nomeFor.push(nome)
+    }
+  }
+  // console.log(nomeFor);
+  nomeFinal = nomeFor.join(' ')
+  console.log(nomeFinal);
+}
+
+ola('Renan')
