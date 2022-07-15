5ec4de07ef84a381ac5505ff51396393127ff40a
Renan Storrer <storrerrenan@gmail.com>
2022-07-15 00:34:23 -0300

    adiciona a contagem na tela

--- fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/ex03/script.js
+++ fundamentos/bloco-07-introducao-a-js-es6-e-testes-unitarios/dia-1-jses6-let-const-arrowfunctions-e-templatesliterals/ex03/script.js
@@ -1,11 +1,20 @@
 const button = document.querySelector('button')
-console.log(button);
 button.addEventListener('click', contador);
+button.addEventListener('click', contM)
 // let contagem = 0;
 
 let contagem = 0
 function contador() {
   contagem = contagem + 1
-  console.log(contagem);
-  // return contagem
+  // console.log(contagem);
+  return contagem
+}
+// console.log(contagem);
+let pContagem = document.querySelector('#pContagem')
+// console.log(pContagem.innerHTML
+
+function contM () {
+  let contagemMostrada = contagem
+  console.log(contagemMostrada);
+  pContagem.innerHTML = contagemMostrada
 }
