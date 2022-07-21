75556ce95789b2e2538c9907d479b7fc0d0bd6ff
Renan Storrer <storrerrenan@gmail.com>
2022-07-21 19:13:56 -0300

    exercicio 2

--- fundamentos/bloco-08-higher-order-functions-js-es6/dia-1-js-es6-introducao-a-hof/script.js
+++ fundamentos/bloco-08-higher-order-functions-js-es6/dia-1-js-es6-introducao-a-hof/script.js
@@ -1,3 +1,5 @@
+// exercicio 1
+
 const newEmployees = (employeeGenerator) => {
   const employees = {
     id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
@@ -13,3 +15,27 @@ const employeeGenerator = (fullName) => {
 }
 
 console.log(newEmployees(employeeGenerator));
+
+// exercicio 2
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado final: ${contador} pontos`;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
