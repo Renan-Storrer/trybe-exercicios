e2b4eb7218cdbfd0e83cff3f23126a0e811463ac
Renan Storrer <storrerrenan@gmail.com>
2022-07-21 19:12:34 -0300

    exercicio 1

--- fundamentos/bloco-08-higher-order-functions-js-es6/dia-1-js-es6-introducao-a-hof/script.js
+++ fundamentos/bloco-08-higher-order-functions-js-es6/dia-1-js-es6-introducao-a-hof/script.js
@@ -1,8 +1,15 @@
-const newEmployees = () => {
+const newEmployees = (employeeGenerator) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: employeeGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  return {fullName, email: `${email}@betrybe.com`};
+}
+
+console.log(newEmployees(employeeGenerator));
