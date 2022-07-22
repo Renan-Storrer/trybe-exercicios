02ac1f0a9cc2a5e63caea10f0e63c4e266bee042
Renan Storrer <storrerrenan@gmail.com>
2022-07-22 19:28:44 -0300

    exercicio 2

--- fundamentos/bloco-08-higher-order-functions-js-es6/dia-2-forEach-find-some-every-sort/exercise2.js
+++ fundamentos/bloco-08-higher-order-functions-js-es6/dia-2-forEach-find-some-every-sort/exercise2.js
@@ -62,10 +62,16 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
 function smallerName() {
   let nameBook;
   // escreva aqui o seu código
 
+  books.forEach((book) => {
+    if (!nameBook || book.name.length < nameBook.length) {
+      nameBook = book.name;
+    }
+  });
   // Variável nameBook que receberá o valor do menor nome;
   return nameBook;
 }
