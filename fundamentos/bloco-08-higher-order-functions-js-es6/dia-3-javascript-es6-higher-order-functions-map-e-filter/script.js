db8381d61a32f6e4c3e65e0daf35a550ab8ac584
Renan Storrer <storrerrenan@gmail.com>
2022-07-25 18:35:21 -0300

    exercicio 1

--- fundamentos/bloco-08-higher-order-functions-js-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
+++ fundamentos/bloco-08-higher-order-functions-js-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/script.js
@@ -62,3 +62,30 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+//---------------------------------------------------------------------------
+
+// EXERCICIO 1
+
+// const expectedResult = [
+//   'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+//   'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+//   'Fundação - Ficção Científica - Isaac Asimov',
+//   'Duna - Ficção Científica - Frank Herbert',
+//   'A Coisa - Terror - Stephen King',
+//   'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+// ];
+
+function formatedBookNames(booksAll) {
+  return booksAll.map((book) => {
+    return `${book.name} - ${book.genre} - ${book.author.name}`
+  })
+}
+
+console.log(formatedBookNames(books));
+
+//-------------------------------------------------------------------------------
+
+// EXERCICIO 2
+
+
