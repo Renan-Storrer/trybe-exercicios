a542f469c79e39f05236135a01cc1e428a5053b1
Renan Storrer <storrerrenan@gmail.com>
2022-07-27 19:18:58 -0300

    exercicio 2

--- fundamentos/bloco-08-higher-order-functions-js-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-js-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
@@ -9,5 +9,15 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
   rectangleArea(...rectangle) // altere a chamada da funcao rectangleArea
-  console.log(rectangle[0] * rectangle[1]);
+  // console.log(rectangle[0] * rectangle[1]);
 });
+
+// EXERCICIO 2
+
+const sum = (...array) => {
+  return array.reduce((contador, numeroAtual) => {
+    return contador + numeroAtual
+  }, 0)
+}
+
+// console.log(sum(10, 20, 30, 40, 50));
