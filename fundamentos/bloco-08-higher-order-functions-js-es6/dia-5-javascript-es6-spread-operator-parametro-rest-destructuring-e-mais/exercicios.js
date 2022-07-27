aac2125a9e2a108fcaee9dcb495c793e071b8158
Renan Storrer <storrerrenan@gmail.com>
2022-07-27 19:25:15 -0300

    exercicio 3

--- fundamentos/bloco-08-higher-order-functions-js-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
+++ fundamentos/bloco-08-higher-order-functions-js-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicios.js
@@ -21,3 +21,29 @@ const sum = (...array) => {
 }
 
 // console.log(sum(10, 20, 30, 40, 50));
+
+// EXERCICIO 3
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+
+// complete a assinatura da função abaixo
+const personLikes = (nome) => {
+  const { name, age ,likes } = nome
+  return `${name} is ${age} years old and likes ${likes.join(', ')}.`
+}
+
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
