4880d562366457ffe9487cc11f282d348dee76ff
Renan Storrer <storrerrenan@gmail.com>
2022-08-16 19:00:02 -0300

    Exercicio 1 - react

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-exercicio-react/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-exercicio-react/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const tarefas = ['Acordar', 'Comer', 'Estudar'];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {tarefas.map(tarefa => Task(tarefa)) }
+    </ul>
   );
 }
 
