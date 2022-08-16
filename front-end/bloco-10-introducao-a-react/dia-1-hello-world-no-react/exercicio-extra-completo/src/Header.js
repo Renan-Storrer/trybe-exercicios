8b55a54f69fbd45fc66c3dd04becfbfefd310136
Renan Storrer <storrerrenan@gmail.com>
2022-08-16 19:44:32 -0300

    exercicio extra

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-extra-completo/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title">Conteúdos de Frontend</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
