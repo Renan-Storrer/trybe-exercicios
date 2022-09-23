b498f2c82788780777387fc07be8895843f69519
Renan Storrer <storrerrenan@gmail.com>
2022-09-23 01:53:56 -0300

    ex 03

--- front-end/bloco-15-redux/dia-1-introducao-a-redux/script.js
+++ front-end/bloco-15-redux/dia-1-introducao-a-redux/script.js
@@ -1,7 +1,15 @@
-// const store = Redux.createStore();
-
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+      };
+    case 'PREVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === 0 ? state.colors.length - 1 : state.index - 1,
+      };
     default:
       return state;
   }
