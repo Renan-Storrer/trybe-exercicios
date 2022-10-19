d86d7ba6fc65c18f6caf70251d848e0ac4d2eeed
Renan Storrer <storrerrenan@gmail.com>
2022-10-19 02:17:44 -0300

    skajs

--- 02-Front-end/bloco-17-context-api-e-react-hooks/dia-3-hooks-e-user-efect/myapp/src/App.js
+++ 02-Front-end/bloco-17-context-api-e-react-hooks/dia-3-hooks-e-user-efect/myapp/src/App.js
@@ -1,11 +1,36 @@
+import { useState } from 'react';
+
 import './App.css';
 
+import messagesList from './data/messagesList';
+
+import List from './components/List';
+
 function App() {
+  const [messages, setMessages] = useState(messagesList);
+
+  const setMessageStatus = (messageId, newStatus) => {
+    const updatedMessages = messages.map((currentMessage) => {
+      if (currentMessage.id === messageId) {
+        return { ...currentMessage, status: newStatus };
+      }
+      return currentMessage;
+    });
+
+    setMessages(updatedMessages);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>TrybeMail</h1>
       </header>
+
+      <List
+        messages={ messages }
+        setMessageStatus={ setMessageStatus }
+      />
+      
     </div>
   );
 }
