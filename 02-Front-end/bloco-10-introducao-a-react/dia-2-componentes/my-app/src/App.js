234d1848789f4770cc278e7172c71cc7c9f23739
Renan Storrer <storrerrenan@gmail.com>
2022-11-10 04:10:46 -0300

    testando

--- 02-Front-end/bloco-10-introducao-a-react/dia-2-componentes/my-app/src/App.js
+++ 02-Front-end/bloco-10-introducao-a-react/dia-2-componentes/my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function handleClick() {
+  console.log('Clicou no botão!')
 }
 
-export default App;
+class App extends React.Component {
+  render() {
+    return (
+      <button onClick={handleClick}>Meu Botão</button>
+    )
+  }
+}
+
+export default App
