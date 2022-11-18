de4e0e7a02e2a6f3182c174239221f0b3ded9225
Renan Storrer <storrerrenan@gmail.com>
2022-11-18 15:40:01 -0300

    test concluido simulando apiCall

--- 02-Front-end/bloco-13-testes-automatizados-com-react-testing-libray/dia-2-mocks-e-inputs/testApiCall/api-call/src/App.test.js
+++ 02-Front-end/bloco-13-testes-automatizados-com-react-testing-libray/dia-2-mocks-e-inputs/testApiCall/api-call/src/App.test.js
@@ -3,6 +3,16 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 it('Verifica se, quando eu recebo uma piada, mostro ele na tela', async () => {
+  global.fetch = (url) => {
+    return Promise.resolve({
+      json: () => Promise.resolve({
+        id: '7h3oGtrOfxc',
+        joke: 'Whiteboards ... are remarkable.',
+        status: 200,
+      })
+    });
+  }
+  
   render(<App />);
   const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
   expect(renderedJoke).toBeInTheDocument();
