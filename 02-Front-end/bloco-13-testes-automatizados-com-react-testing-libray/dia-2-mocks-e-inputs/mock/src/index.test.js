440486dbf95ffd9b49f4caa59ebb4932b5560c27
Renan Storrer <storrerrenan@gmail.com>
2022-11-18 00:49:54 -0300

    testes

--- 02-Front-end/bloco-13-testes-automatizados-com-react-testing-libray/dia-2-mocks-e-inputs/mock/src/index.test.js
+++ 02-Front-end/bloco-13-testes-automatizados-com-react-testing-libray/dia-2-mocks-e-inputs/mock/src/index.test.js
@@ -14,15 +14,27 @@ describe("Aprendendo sobre mocks", () => {
 
   it("Testa se a função foi chamada", () => {
 
+    somaDoisNumeros = jest.fn();
+    somaDoisNumeros();
     expect(somaDoisNumeros).toHaveBeenCalled();
   });
 
   it("Testa o resultado da função", () => {
 
+    somaDoisNumeros = jest.fn().mockReturnValue(10);
+  
+    expect(somaDoisNumeros()).toBe(10)
   });
 
-  it("Testa quantas vezes a função oi chamada", () => {
+  it("Testa quantas vezes a função foi chamada", () => {
 
+    somaDoisNumeros = jest.fn()
+    somaDoisNumeros();
+    somaDoisNumeros();
+    somaDoisNumeros();
+    somaDoisNumeros();
+
+    expect(somaDoisNumeros).toHaveBeenCalledTimes(4);
   });
 
   it("Testa se retorna true quando o numero é par e false quando o numero é impar", () => {
