a9565d95a3e8dbfede0b27fb35e0ce349b9f227b
Renan Storrer <storrerrenan@gmail.com>
2022-11-29 15:54:12 -0300

    att

--- 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/index.js
+++ 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/index.js
@@ -1,3 +1,11 @@
+import { legacy_createStore as createStore } from "redux";
+
+const INITIAL_STATE =  { count: 0}
+const reducer = (state = INITIAL_STATE, action) => {
+  return state;
+}
+const store = createStore(reducer)
+
 const incrementButton = document.querySelector("button")
 incrementButton.addEventListener("click", () => {
   console.log('click');
