b9918eb3f1fa3809f181cb5bdfdb80753f34e656
Renan Storrer <storrerrenan@gmail.com>
2022-11-30 15:22:21 -0300

    ..

--- 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/my-app/src/App.js
+++ 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>0</h2>
+        <button>Incrementa 1</button>
+        <button>Incrementa 5</button>
+      </div>
+    );
+  }
 }
 
 export default App;
