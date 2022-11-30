0b11a1c5245004238ec1857b4a11eacfa005e486
Renan Storrer <storrerrenan@gmail.com>
2022-11-30 15:24:55 -0300

    att

--- 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/my-app/src/App.js
+++ 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-2-redux-no-react/my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 class App extends React.Component {
   render() {
+    const { countState } = this.props;
     return (
       <div>
         <h1>Contador</h1>
-        <h2>0</h2>
+        <h2>{countState}</h2>
         <button>Incrementa 1</button>
         <button>Incrementa 5</button>
       </div>
@@ -13,4 +15,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  countState: state.count,
+});
+
+export default connect(mapStateToProps)(App);
