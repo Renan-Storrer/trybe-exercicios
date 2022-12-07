f5bd7387eb640aa93fb1ca8bbcbc7335ea85fb06
Renan Storrer <storrerrenan@gmail.com>
2022-12-07 02:49:46 -0300

    pronto

--- 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-3-actions-assincronas/game-of-thunks/src/App.js
+++ 02-Front-end/bloco-14-gerenciamento-de-estado-com-redux/dia-3-actions-assincronas/game-of-thunks/src/App.js
@@ -1,12 +1,37 @@
 import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { thunkCharacter } from './redux/actions';
+import CharacterCard from './components/CharacterCard';
 
-function App() {
-  return (
-    <div className="App">
-     <input type='text' placeholder='pesquisar'></input>
-     <button>PESQUISAR</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      character: ''
+    }
+  }
+
+  render() {
+    const { character } = this.state;
+    const { dispatch } = this.props;
+    return (
+      <>
+        <input
+          type="text"
+          onChange={ ({ target }) => this.setState({ character: target.value }) }
+          value={ character }
+        />
+        <button
+          type="button"
+          onClick={ () => dispatch(thunkCharacter(character)) }
+        >
+          Search
+        </button>
+        <CharacterCard />
+      </>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
