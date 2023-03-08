e892c95e4c7c8abe94fc3996691d6213b4d90a78
Renan Storrer <storrerrenan@gmail.com>
2023-03-08 16:27:57 -0300

    .

--- 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-API-REST-com-express/ex-fixação/soccer-team-manager/src/app.js
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-API-REST-com-express/ex-fixação/soccer-team-manager/src/app.js
@@ -1,7 +1,45 @@
 const express = require('express');
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+]
+
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
+});
 
 module.exports = app;
