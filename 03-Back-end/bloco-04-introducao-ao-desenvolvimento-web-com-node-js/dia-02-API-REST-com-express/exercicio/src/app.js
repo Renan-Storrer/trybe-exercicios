f1d692610cd4fc0d9598b57d3dfea83646e77053
Renan Storrer <storrerrenan@gmail.com>
2023-03-09 11:23:42 -0300

    att

--- 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-API-REST-com-express/exercicio/src/app.js
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-API-REST-com-express/exercicio/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pode ser lido: ${error}`);
+  } 
+};
 
 module.exports = app;
