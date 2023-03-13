ab9bca18a62f0e4151402fee96d39aabf33243fc
Renan-Storrer <storrerrenan@gmail.com>
2023-03-13 17:51:09 -0300

    att

--- 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/hello-word/index.js
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/hello-word/index.js
@@ -1,3 +1,6 @@
-const path = require('path')
+const readline = require('readline-sync');
 
-console.log(path.dirname('/usr/share/gnome'));
+const name = readline.question('Qual seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
