64f8bf3dba4144912b79f93c50545fc6f4461378
Renan-Storrer <storrerrenan@gmail.com>
2023-03-13 18:25:47 -0300

    att

--- 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/exercicio-do-dia/bmi.js
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/exercicio-do-dia/bmi.js
@@ -17,6 +17,32 @@ function main() {
 
   const bmi = handleBMI(weight, height);
 
+  if (bmi < 18.5) {
+    console.log('Status: Underweight (thin)');
+    return;
+  }
+
+  if (bmi >= 18.5 && bmi < 25) {
+    console.log('Status: Normal weight');
+    return;
+  }
+
+  if (bmi >= 25 && bmi < 30) {
+    console.log('Status: Overweight');
+    return;
+  }
+
+  if (bmi >= 30 && bmi < 35) {
+    console.log('Status: Grade I obesity');
+    return;
+  }
+
+  if (bmi >= 35 && bmi < 40) {
+    console.log('Status: Grade II obesity');
+    return;
+  }
+
+  console.log('Status: Obesity grades III and IV');
   console.log(`BMI: ${bmi.toFixed(2)}`);
 }
 
