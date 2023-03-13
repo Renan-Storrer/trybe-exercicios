cfb886d39252ed88abdda710db50b7521b0aab4f
Renan-Storrer <storrerrenan@gmail.com>
2023-03-13 18:24:38 -0300

    att

--- 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/exercicio-do-dia/bmi.js
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-01-node-js-runtime-assincrono/exercicio-do-dia/bmi.js
@@ -1,5 +1,4 @@
-const weightInKg = 80; // Você pode utilizar o valor que desejar aqui
-const heightInCm = 178; // Você pode utilizar o valor que desejar aqui
+const readline = require('readline-sync');
 
 function handleBMI(weight, height) {
   console.log(`Weight: ${weight}, Height: ${height}`);
@@ -7,14 +6,16 @@ function handleBMI(weight, height) {
   const heightInMeters = height / 100;
   const heightSquared = heightInMeters ** 2;
 
-  const bmi = weight / heightSquared;
+  const bmi = (weight / heightSquared);
 
   return bmi;
 }
 
-// A função main é o ponto de partida do nosso programa
 function main() {
-  const bmi = handleBMI(weightInKg, heightInCm);
+  const weight = readline.questionFloat('What\'s your weight? (kg) ');
+  const height = readline.questionInt('What\'s your height? (cm) ');
+
+  const bmi = handleBMI(weight, height);
 
   console.log(`BMI: ${bmi.toFixed(2)}`);
 }
