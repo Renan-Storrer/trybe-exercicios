53d9227c3d6ad54c9efe9109046541fce43a6707
Renan-Storrer <storrerrenan@gmail.com>
2023-03-13 16:01:48 -0300

    chore: refatora server.js

--- 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-05-express-com-mysql/trybecash-api/src/server.js
+++ 03-Back-end/bloco-04-introducao-ao-desenvolvimento-web-com-node-js/dia-05-express-com-mysql/trybecash-api/src/server.js
@@ -1,14 +1,7 @@
 const app = require('./app');
-const connection = require('./db/connection');
 
 const PORT = 3001;
 
-app.listen(PORT, async () => {
+app.listen(PORT, () => {
   console.log(`API TrybeCash está sendo executada na porta ${PORT}`);
-
-  // O código abaixo é para testarmos a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
 });
