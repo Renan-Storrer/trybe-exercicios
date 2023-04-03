31f59b571bc7e4a8cf472201c007547b5dea7006
Renan-Storrer <storrerrenan@gmail.com>
2023-04-03 15:41:19 -0300

    configura seeder address

--- 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-02-associantions-1-1-e-n/exemplos/src/seeders/20230403183846-addresses.js
+++ 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-02-associantions-1-1-e-n/exemplos/src/seeders/20230403183846-addresses.js
@@ -1,24 +1,43 @@
-'use strict';
-
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert('addresses',
+      [
+        {
+          city: 'Belo Horizonte',
+          street: 'Rua Flórida',
+          number: 1080,
+          employee_id: 1,
+        },
+        {
+          city: 'São Paulo',
+          street: 'Avenida Paulista',
+          number: 1980,
+          employee_id: 2,
+        },
+        {
+          city: 'Fortaleza',
+          street: 'Rua das Enseadas',
+          number: 95,
+          employee_id: 3,
+        },
+        {
+          city: 'Belo Horizonte',
+          street: 'Rua Andaluzita',
+          number: 131,
+          employee_id: 4,
+        },
+        {
+          city: 'Curitiba',
+          street: 'Rua Fria',
+          number: 101,
+          employee_id: 4,
+        },
+      ],
+      {},
+    );
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkDelete('addresses', null, {});
+  },
 };
