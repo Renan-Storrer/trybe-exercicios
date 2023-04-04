cbec1b92eebb85be5b4b44807d59b0eb423a4980
Renan-Storrer <storrerrenan@gmail.com>
2023-04-04 15:26:24 -0300

    feat: configura inicialmente o migration book

--- 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-03-association-nn/books-api-sequelize/src/migrations/20230404182438-create-books.js
+++ 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-03-association-nn/books-api-sequelize/src/migrations/20230404182438-create-books.js
@@ -1,21 +1,30 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      releaseYear: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'release_year', // só precisamos definir explicitamente os campos com nomes compostos, por causa da semelhança entre camelCase e snake_case
+      },
+      totalPages: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        field: 'total_pages', // ambos começam com letra minúscula, quando temos duas palavras que existe diferença
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('books');
+  },
 };
