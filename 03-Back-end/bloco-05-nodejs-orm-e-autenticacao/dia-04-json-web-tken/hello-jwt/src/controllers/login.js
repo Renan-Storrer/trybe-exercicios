8a01df238948e1ea243bcb830607a7320dcc6348
Renan-Storrer <storrerrenan@gmail.com>
2023-04-06 01:42:28 -0300

    feat: cria login.js com suas funcionalidades

---
+++ 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-04-json-web-tken/hello-jwt/src/controllers/login.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const validateBody = (body) => {
+  Joi.object({
+    username: Joi.string().min(5).alphanum().required().messages({
+      'string.min': '"username" length must be 5 characters long',
+      'string.required': '"username" is required',
+    }),
+    password: Joi.string().min(5).required().messages({
+      'string.min': '"password" length must be 5 characters long',
+      'string.required': '"password" is required',
+    }),
+  }).validate(body);
+}
+
+module.exports = async (req, res, next) => {
+  const { error } = validateBody(req.body);
+
+  if (error) return next(error)
+}
