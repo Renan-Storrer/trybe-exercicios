37da6e93e4c9dc5f222a9a740470436943114076
Renan-Storrer <storrerrenan@gmail.com>
2023-04-06 01:50:22 -0300

    att

--- 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-04-json-web-tken/hello-jwt/src/controllers/login.js
+++ 03-Back-end/bloco-05-nodejs-orm-e-autenticacao/dia-04-json-web-tken/hello-jwt/src/controllers/login.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 
 const { JWT_SECRET } = process.env;
 
-const validateBody = (body) => {
+const validateBody = (body) =>
+  /* Utilizamos o Joi para validar o schema do body */
   Joi.object({
     username: Joi.string().min(5).alphanum().required().messages({
       'string.min': '"username" length must be 5 characters long',
@@ -14,16 +15,21 @@ const validateBody = (body) => {
       'string.required': '"password" is required',
     }),
   }).validate(body);
-}
 
 module.exports = async (req, res, next) => {
   const { error } = validateBody(req.body);
+  /* Caso ocorra erro na validação do Joi, passamos esse */
+  /* erro para o express, que chamará nosso middleware de erro */
+  if (error) return next(error);
 
-  if (error) return next(error)
+const payload = {
+  username: req.body.username,
+  admin: false,
+};
 
-  const token = jwt.sign(payload, JWT_SECRET, {
-    expiresIn: '1h',
-  })
+const token = jwt.sign(payload, JWT_SECRET, {
+  expiresIn: '1h',
+});
 
-  res.status(200).json({ token })
-}
+res.status(200).json({ token });
+};
