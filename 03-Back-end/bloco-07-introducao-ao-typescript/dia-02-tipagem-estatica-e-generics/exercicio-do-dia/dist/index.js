aef75e6578a403ef1f995d2ad4162a4efdd9c7fd
Renan-Storrer <storrerrenan@gmail.com>
2023-04-19 19:03:48 -0300

    feat: requisito 4

---
+++ 03-Back-end/bloco-07-introducao-ao-typescript/dia-02-tipagem-estatica-e-generics/exercicio-do-dia/dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const gol = new Car('Volkswage', 'prata', 4);
+// gol.turnOn();
+// gol.speedUp();
+// gol.speedDown();
+// gol.turn("esquerda");
+// gol.speedUp();
+// gol.speedDown();
+// gol.turn("direita");
+// gol.speedUp();
+// gol.speedDown();
+// gol.turn("direita");
+// gol.speedDown();
+// gol.stop();
+// gol.speedUp();
+// gol.speedDown();
+// gol.turn("direita");
+// gol.speedUp();
+// gol.speedDown();
+// gol.turn("esquerda");
+// gol.speedUp();
+// gol.speedDown();
+// gol.turn("direita");
+// gol.speedDown();
+// gol.stop();
+// gol.speedUp();
+const calabresa = {
+    flavor: "Calabresa",
+    slices: 8
+};
+console.log(calabresa);
+const marguerita = {
+    flavor: "Marguerita",
+    slices: 6
+};
+console.log(marguerita);
+const nutella = {
+    flavor: "Nutella",
+    slices: 4
+};
+console.log(nutella);
