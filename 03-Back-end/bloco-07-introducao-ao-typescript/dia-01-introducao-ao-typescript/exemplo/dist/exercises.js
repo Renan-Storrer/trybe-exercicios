01363e4f908e1e9f26711c9dedcedaaafab50b7a
Renan-Storrer <storrerrenan@gmail.com>
2023-04-18 21:25:11 -0400

    att

--- 03-Back-end/bloco-07-introducao-ao-typescript/dia-01-introducao-ao-typescript/exemplo/dist/exercises.js
+++ 03-Back-end/bloco-07-introducao-ao-typescript/dia-01-introducao-ao-typescript/exemplo/dist/exercises.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.triangleCheck = exports.getPolygonPerimeter = exports.getTriangleArea = exports.getRectangleArea = exports.getSquareArea = void 0;
+exports.getCircleArea = exports.getTrapezoidArea = exports.getRhombusArea = exports.triangleCheck = exports.getPolygonPerimeter = exports.getTriangleArea = exports.getRectangleArea = exports.getSquareArea = void 0;
 function getSquareArea(side) {
     return side ** 2;
 }
@@ -28,4 +28,19 @@ function triangleCheck(sideA, sideB, sideC) {
     return checkSideA && checkSideB && checkSideC;
 }
 exports.triangleCheck = triangleCheck;
+;
+function getRhombusArea(D, d) {
+    return (d * D) / 2;
+}
+exports.getRhombusArea = getRhombusArea;
+;
+function getTrapezoidArea(h, B, b) {
+    return ((B + b) * h) / 2;
+}
+exports.getTrapezoidArea = getTrapezoidArea;
+;
+function getCircleArea(radius) {
+    return Math.PI * radius ** 2;
+}
+exports.getCircleArea = getCircleArea;
 ;
