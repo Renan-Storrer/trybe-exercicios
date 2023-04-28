eb9a07582aec9f11bb9785028f706306f1cfe6a9
Renan-Storrer <storrerrenan@gmail.com>
2023-04-28 04:16:59 -0300

    feat: app.use

--- 03-Back-end/bloco-07-introducao-ao-typescript/dia-03-express-com-typescript/express-typescript/index.ts
+++ 03-Back-end/bloco-07-introducao-ao-typescript/dia-03-express-com-typescript/express-typescript/index.ts
@@ -1,6 +1,6 @@
 // ./index.ts
 
-import express, { Request, Response } from 'express';
+import express, { NextFunction ,Request, Response } from 'express';
 import statusCodes from './statusCodes';
 
 const app = express();
@@ -11,6 +11,31 @@ const PORT = 8000;
 
 app.get('/', (_req: Request, res: Response) => {
     res.status(statusCodes.OK).send('Express + TypeScript')
+});
+
+app.use((err: Error, _req: Request, res: Response, next: NextFunction) => {
+  const { name, message, details } = err as any;
+  console.log(`name: ${name}`);
+
+  switch (name) {
+    case 'BadRequestError':
+      res.status(400).json({ message });
+      break;
+    case 'ValidationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+    case 'NotFoundError':
+      res.status(404).json({ message });
+      break;
+    case 'ConflictError':
+      res.status(409).json({ message });
+      break;
+    default:
+      console.error(err);
+      res.sendStatus(500);
+  }
+
+  next();
 });
 
 app.listen(PORT, () => {
