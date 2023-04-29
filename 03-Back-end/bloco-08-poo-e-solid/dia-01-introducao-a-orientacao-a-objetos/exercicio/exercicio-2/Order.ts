2628ed68054b3a5f18780a1c636b4a094c60512c
Renan-Storrer <storrerrenan@gmail.com>
2023-04-29 20:50:57 -0300

    feat: exercicio 4

--- 03-Back-end/bloco-08-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicio/exercicio-2/Order.ts
+++ 03-Back-end/bloco-08-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercicio/exercicio-2/Order.ts
@@ -1,6 +1,7 @@
 // Order.ts
 import Client from './Client';
 import OrderItem from './OrderItem';
+
 export default class Order {
   private _client: Client;
   private _items: OrderItem[] = [];
@@ -58,4 +59,31 @@ export default class Order {
 
     this._discount = value;
   }
+
+  calculateTotal(): number {
+    return this.items
+      .reduce((previousValue, item) => {
+        const total = previousValue + item.price;
+
+        return total;
+      }, 0);
+  }
+
+  calculateTotalWithDiscount(): number {
+    return this.calculateTotal() * (1 - this.discount);
+  }
 }
+
+// Para testar!
+
+const client = new Client('João');
+
+const sandwich = new OrderItem('Sanduíche Natural', 5.00);
+const juice = new OrderItem('Suco de Abacaxi', 5.00);
+const dessert = new OrderItem('Gelatina de Uva', 2.50);
+
+const order = new Order(client, [sandwich, juice, dessert], 'dinheiro', 0.10);
+
+console.log(order);
+console.log('Valor normal: ', order.calculateTotal());
+console.log('Valor com desconto: ', order.calculateTotalWithDiscount());
