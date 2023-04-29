464ca353687ac9fabbe8ae9aff529a226ae018e2
Renan-Storrer <storrerrenan@gmail.com>
2023-04-29 19:01:51 -0300

    feat:altera visibilidade dos atributos na classe Tv

--- 03-Back-end/bloco-08-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exemplos/sintaxe.ts
+++ 03-Back-end/bloco-08-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exemplos/sintaxe.ts
@@ -1,24 +1,24 @@
 class Tv {
-  //atributos
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string[];
+  private _connectedTo?: string;
 
-  constructor(b: string, s: number, r: string, c: string[]) {
-    this.brand = b;
-    this.size = s;
-    this.resolution = r;
-    this.connections = c;
+  constructor(brand: string, size: number, resolution: string, connections: string[]) {
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
   }
 
-  turnOn(): void {
-    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution} \n\
-    available connections: ${this.connections}`
+  turnOn():void {
+    console.log(
+      `TV ${this._brand}, ${this._size}", resolution: ${this._resolution} \n\
+available connections: ${this._connections}`,
     );
   }
 }
 
-const tv1 = new Tv('LG', 32,'4k', ['HDMI', 'Ethernet', 'Wi-Fi']);
+const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wi-Fi']);
 tv1.turnOn();
