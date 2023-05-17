ef164426cc943350fdc63347776eb4da71935380
Renan-Storrer <storrerrenan@gmail.com>
2023-05-17 03:41:49 -0300

    fix: remove erro lint de main.js

--- 02-Front-end/bloco-01-introducao-ao-frontend-e-js-assincrono/dia-01-ambiente-de-desenvolvimento/exercicio/main.js
+++ 02-Front-end/bloco-01-introducao-ao-frontend-e-js-assincrono/dia-01-ambiente-de-desenvolvimento/exercicio/main.js
@@ -5,14 +5,16 @@ const button = document.querySelector('#button');
 const seletor = document.querySelector('#option');
 const textoDeSaida = document.querySelector('#answer');
 
-button.addEventListener('click', (event) => {
-  event.preventDefault();
+const UUID_VERSION = 4
 
-  const campos = {
+button.addEventListener('click', (event) => {
+   event.preventDefault();
+
+   const campos = {
     cpf: validator.isTaxID(campoDeTexto.value, 'pt-BR'),
     hexColor: validator.isHexColor(campoDeTexto.value),
     email: validator.isEmail(campoDeTexto.value),
-    uuid: validator.isUUID(campoDeTexto.value, 4),
+    uuid: validator.isUUID(campoDeTexto.value, UUID_VERSION),
     url: validator.isURL(campoDeTexto.value),
   };
 
