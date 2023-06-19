c351369bc968a1d67741bb3e1c246b3956042eed
Renan-Storrer <storrerrenan@gmail.com>
2023-06-19 19:34:32 -0300

    feat: exercicio2

---
+++ 04-ciencia-da-computacao/bloco-03-estruturas-de-dados-I-listas-lineares/dia-03-no-e-listas-encadeadas/exercicio2.py
@@ -0,0 +1,9 @@
+def index_of(self, value):
+    position = 1
+    current_value = self.head_value
+    while current_value:
+        if current_value.value == value:
+            return position
+        current_value = current_value.next
+        position += 1
+    return -1
