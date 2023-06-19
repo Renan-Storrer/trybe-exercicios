d456c6b9370bfbde9fe5ba3f4b4240acf9f3f76b
Renan-Storrer <storrerrenan@gmail.com>
2023-06-19 19:36:59 -0300

    feat: exercicio2

---
+++ 04-ciencia-da-computacao/bloco-03-estruturas-de-dados-I-listas-lineares/dia-04-pilhas/exercicio2.py
@@ -0,0 +1,17 @@
+class Stack:
+
+    def __init__(self):
+        self.__data = LinkedList()
+
+    def is_empty(self):
+        return not len(self.__data)
+
+    def push(self, value):
+        self.__data.insert_last(value)
+
+    def pop(self):
+        return self.__data.remove_last()
+
+    def peek(self):
+        return self.__data.get_element_at(len(self.__data))
+    
