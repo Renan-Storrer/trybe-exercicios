a24272cff3e10fd0b0112a1de5f03d2dde71d505
Renan-Storrer <storrerrenan@gmail.com>
2023-06-19 19:33:31 -0300

    feat: exercicio1

--- 04-ciencia-da-computacao/bloco-03-estruturas-de-dados-I-listas-lineares/dia-03-no-e-listas-encadeadas/exercicio1.py
+++ 04-ciencia-da-computacao/bloco-03-estruturas-de-dados-I-listas-lineares/dia-03-no-e-listas-encadeadas/exercicio1.py
@@ -6,3 +6,11 @@ def clear(self):
 def clear(self):
     while not self.is_empty():
         self.remove_first()
+
+def __get_node_at(self, position):
+    value_to_be_returned = self.head_value
+    if value_to_be_returned:
+        while position > 0 and value_to_be_returned.next:
+            value_to_be_returned = value_to_be_returned.next
+            position -= 1
+    return value_to_be_returned
